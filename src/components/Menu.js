24dfc11bed3d835360489fa01d05efc49b167834
saradinismarques <sara.filipadinis@gmail.com>
2024-07-17 17:36:56 +0100

    text and gradient on learn page

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -7,12 +7,12 @@ const Menu = () => {
     <div className="bottom-menu">
       <Link to="/" className="menu-button">HOME</Link>
       <Link to="/learn" className="menu-button">LEARN</Link>
-      <button className="menu-button">GET INSPIRED</button>
-      <button className="menu-button">CONTEXTUALIZE</button>
-      <button className="menu-button">ANALYZE</button>
-      <button className="menu-button">IDEATE</button>
-      <button className="menu-button">COMPARE</button>
-      <button className="menu-button">+</button>
+      <Link to="/" className="menu-button">GET INSPIRED</Link>
+      <Link to="/" className="menu-button">CONTEXTUALIZE</Link>
+      <Link to="/" className="menu-button">ANALYZE</Link>
+      <Link to="/" className="menu-button">IDEATE</Link>
+      <Link to="/" className="menu-button">COMPARE</Link>
+      <Link to="/" className="menu-button">+</Link>
     </div>
   );
 };
--- src/pages/LearnPage.js
+++ src/pages/LearnPage.js
@@ -1,13 +1,17 @@
 // src/components/pages/LearnPage.js
 
 import React from 'react';
-import Circle from '../components/Circle'
-import '../styles/LearnPage.css';
+import '../styles/Text.css'
 
 const LearnPage = () => {
   return (
-    <div>
+    <div className="gradient-background">
         <div className="circle left"></div>
+        <div className="text-container">
+            <h2>PRINCIPLE 7</h2>
+            <h1>THE OCEAN IS LARGELY UNKNOWN</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
     </div>
   );
 };
