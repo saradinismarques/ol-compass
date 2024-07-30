c0d4743ec0672b1354a1e92e980cf0772c038b51
saradinismarques <sara.filipadinis@gmail.com>
2024-07-30 12:37:00 +0100

    button black when in that page

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,18 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/Menu.css';
 import { Link } from 'react-router-dom';
 
 const Menu = () => {
+  const [activeButton, setActiveButton] = useState(null);
+
+  const handleClick = (buttonId) => {
+    setActiveButton(buttonId);
+  };
+
   return (
     <div className="bottom-menu">
-      <Link to="/" className="menu-button">HOME</Link>
-      <Link to="/learn" className="menu-button">LEARN</Link>
-      <Link to="/get-inspired" className="menu-button">GET INSPIRED</Link>
-      <Link to="/" className="menu-button">CONTEXTUALIZE</Link>
-      <Link to="/" className="menu-button">ANALYZE</Link>
-      <Link to="/" className="menu-button">IDEATE</Link>
-      <Link to="/" className="menu-button">COMPARE</Link>
-      <Link to="/" className="menu-button">+</Link>
+      <Link 
+        to="/" 
+        className={`menu-button ${activeButton === 'home' ? 'active' : ''}`} 
+        onClick={() => handleClick('home')}
+      >
+        HOME
+      </Link>
+      <Link 
+        to="/learn" 
+        className={`menu-button ${activeButton === 'learn' ? 'active' : ''}`} 
+        onClick={() => handleClick('learn')}
+      >
+        LEARN
+      </Link>
+      <Link 
+        to="/get-inspired" 
+        className={`menu-button ${activeButton === 'get-inspired' ? 'active' : ''}`} 
+        onClick={() => handleClick('get-inspired')}
+      >
+        GET INSPIRED
+      </Link>
+      <Link 
+        to="/" 
+        className={`menu-button ${activeButton === 'contextualize' ? 'active' : ''}`} 
+        onClick={() => handleClick('contextualize')}
+      >
+        CONTEXTUALIZE
+      </Link>
+      <Link 
+        to="/" 
+        className={`menu-button ${activeButton === 'analyze' ? 'active' : ''}`} 
+        onClick={() => handleClick('analyze')}
+      >
+        ANALYZE
+      </Link>
+      <Link 
+        to="/" 
+        className={`menu-button ${activeButton === 'ideate' ? 'active' : ''}`} 
+        onClick={() => handleClick('ideate')}
+      >
+        IDEATE
+      </Link>
+      <Link 
+        to="/" 
+        className={`menu-button ${activeButton === 'compare' ? 'active' : ''}`} 
+        onClick={() => handleClick('compare')}
+      >
+        COMPARE
+      </Link>
+      <Link 
+        to="/" 
+        className={`menu-button ${activeButton === 'plus' ? 'active' : ''}`} 
+        onClick={() => handleClick('plus')}
+      >
+        +
+      </Link>
     </div>
   );
 };
