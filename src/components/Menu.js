fa5d419af5d199411c03b47841bada8bb46e0113
saradinismarques <sara.filipadinis@gmail.com>
2024-10-03 12:31:36 +0100

    [Menu] add show more to menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import '../styles/Menu.css';
 
 const Menu = () => {
   const location = useLocation();
   const currentPath = location.pathname;
+  // Load the initial state from localStorage or set default to false
+  const [showMore, setShowMore] = useState(() => {
+    const storedShowMore = localStorage.getItem('showMore');
+    return storedShowMore === 'true'; // Convert string to boolean
+  });
+
+  // Update localStorage whenever showMore changes
+  useEffect(() => {
+    localStorage.setItem('showMore', showMore.toString());
+  }, [showMore]);
+
+  const toggleShowMore = () => {
+    setShowMore(!showMore);
+  };
 
   // Determine the active button based on the current path
   const getActiveButton = (path) => {
@@ -55,41 +69,49 @@ const Menu = () => {
         GET INSPIRED
       </Link>
       <Link
-        to="/ol-compass/contextualize"
-        className={`menu-button ${activeButton === 'contextualize' ? 'active' : 'disabled'}`}
-      >
-        CONTEXTUALIZE
-      </Link>
-      <Link
         to="/ol-compass/analyze"
         className={`menu-button ${activeButton === 'analyze' ? 'active' : 'disabled'}`}
       >
         ANALYZE
       </Link>
-      <Link
-        to="/ol-compass/ideate"
-        className={`menu-button ${activeButton === 'ideate' ? 'active' : 'disabled'}`}
-      >
-        IDEATE
-      </Link>
-      <Link
-        to="/ol-compass/compare"
-        className={`menu-button ${activeButton === 'compare' ? 'active' : 'disabled'}`}
-      >
-        COMPARE
-      </Link>
-      <Link
+      {showMore && (
+        <>
+          <Link
+            to="/ol-compass/contextualize"
+            className={`menu-button ${activeButton === 'contextualize' ? 'active' : 'disabled'}`}
+          >
+            CONTEXTUALIZE
+          </Link>
+          <Link
+            to="/ol-compass/ideate"
+            className={`menu-button ${activeButton === 'ideate' ? 'active' : 'disabled'}`}
+          >
+            IDEATE
+          </Link>
+          <Link
+            to="/ol-compass/compare"
+            className={`menu-button ${activeButton === 'compare' ? 'active' : 'disabled'}`}
+          >
+            COMPARE
+          </Link>
+        </>
+      )}
+      
+      <button onClick={toggleShowMore} className="menu-show-more-button">
+        {showMore ? 'Show less' : 'Show more'}
+      </button>
+      {/* <Link
         to="/ol-compass/evaluate"
         className={`menu-button ${activeButton === 'evaluate' ? 'active' : 'disabled'}`}
       >
         EVALUATE
-      </Link>
-      <Link
+      </Link> */}
+      {/* <Link
         to="/ol-compass/make-yours"
         className={`menu-button ${activeButton === 'make-yours' ? 'active' : 'disabled'}`}
       >
         MAKE YOURS
-      </Link>
+      </Link> */}
       {/* <Link
         to="/some-other-path"
         className={`menu-button ${activeButton === 'plus' ? 'active' : 'disabled'}`}
